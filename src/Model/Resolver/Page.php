53871238940a173c8ba015236c41ce747ac7eb8c
krystian15 <31726767+krystian15@users.noreply.github.com>
2019-12-11 09:22:22 +0100

    Update Page.php
    Respect store id

--- src/Model/Resolver/Page.php
+++ src/Model/Resolver/Page.php
@@ -2,14 +2,12 @@
 /**
  * ScandiPWA_CmsGraphQl
  *
- * @category    Scandiweb
- * @package     ScandiPWA_CmsGraphQl
- * @author      Kriss Andrejevs <info@scandiweb.com>
- * @copyright   Copyright (c) 2018 Scandiweb, Ltd (https://scandiweb.com)
+ * @category  Scandiweb
+ * @package   ScandiPWA_CmsGraphQl
+ * @author    Kriss Andrejevs <info@scandiweb.com>
+ * @copyright Copyright (c) 2018 Scandiweb, Ltd (https://scandiweb.com)
  */
-
 namespace ScandiPWA\CmsGraphQl\Model\Resolver;
-
 use Magento\CmsGraphQl\Model\Resolver\DataProvider\Page as PageDataProvider;
 use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\Framework\GraphQl\Config\Element\Field;
@@ -19,29 +17,29 @@ use Magento\Framework\GraphQl\Query\Resolver\Value;
 use Magento\Framework\GraphQl\Query\Resolver\ValueFactory;
 use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
 use Magento\Cms\Model\PageFactory;
-
+use Magento\Store\Model\StoreManagerInterface;
 /**
  * Class Page
  * @package ScandiPWA\CmsGraphQl\Model\Resolver
  */
 class Page extends \Magento\CmsGraphQl\Model\Resolver\Page
 {
-
     /**
      * @var PageDataProvider
      */
     private $pageDataProvider;
-
     /**
      * @var ValueFactory
      */
     private $valueFactory;
-
     /**
      * @var PageFactory
      */
     private $pageFactory;
-
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
     /**
      * @param PageDataProvider $pageDataProvider
      * @param ValueFactory $valueFactory
@@ -49,7 +47,8 @@ class Page extends \Magento\CmsGraphQl\Model\Resolver\Page
     public function __construct(
         PageDataProvider $pageDataProvider,
         ValueFactory $valueFactory,
-        PageFactory $pageFactory
+        PageFactory $pageFactory,
+        StoreManagerInterface $storeManager
     ) {
         parent::__construct(
             $pageDataProvider
@@ -57,8 +56,8 @@ class Page extends \Magento\CmsGraphQl\Model\Resolver\Page
         $this->pageDataProvider = $pageDataProvider;
         $this->valueFactory = $valueFactory;
         $this->pageFactory = $pageFactory;
+        $this->storeManager = $storeManager;
     }
-
     /**
      * @inheritdoc
      */
@@ -69,16 +68,13 @@ class Page extends \Magento\CmsGraphQl\Model\Resolver\Page
         array $value = null,
         array $args = null
     ) : Value {
-
         $result = function () use ($args) {
             $pageId = $this->getPageId($args);
             $pageData = $this->getPageData($pageId);
-
             return $pageData;
         };
         return $this->valueFactory->create($result);
     }
-
     /**
      * @param array $args
      * @return int
@@ -86,18 +82,15 @@ class Page extends \Magento\CmsGraphQl\Model\Resolver\Page
      */
     private function getPageId(array $args): int
     {
+        $storeId = $this->storeManager->getStore()->getId();
         if (isset($args['id'])) {
             return (int)$args['id'];
         }
-
         if (isset($args['url_key'])) {
-            return (int)$this->pageFactory->create()->load($args['url_key'])->getId();
+            return (int)$this->pageFactory->create()->setStoreId($storeId)->load($args['url_key'])->setStoreId($storeId)->getId();
         }
-
         throw new GraphQlInputException(__('Page id should be specified'));
-
     }
-
     /**
      * @param int $pageId
      * @return array
@@ -112,5 +105,4 @@ class Page extends \Magento\CmsGraphQl\Model\Resolver\Page
         }
         return $pageData;
     }
-
 }
